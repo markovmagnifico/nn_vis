66a7719af33b80b65e328669a34c5409bc3d34dd
markovmagnifico <markovmagnifico@gmail.com>
2023-08-17 11:53:51 -0700

    Updates to main webpage

--- main.js
+++ main.js
@@ -1,206 +1,25 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
-import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import WebGL from 'three/addons/capabilities/WebGL.js';
-import model from './train.js';
-import convModel from './model.js';
 
-// Create the renderer
+// Set up the scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x62a0de);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Set up the scene and camera
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x62a0de);
+// Set up the camera
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.set(0, -4, 10);
-camera.lookAt(0, 0, 0);
+camera.position.set(0, -2.9, 0.7);
+camera.rotation.set(1, -4.3, 6);
+window.camera = camera;
 const controls = new OrbitControls(camera, renderer.domElement);
-
-// Create a basic green cube
-const geometry1 = new THREE.BoxGeometry(1, 1, 1);
-const material1 = new THREE.MeshBasicMaterial({ color: 0x77ee11 });
-const cube = new THREE.Mesh(geometry1, material1);
-scene.add(cube);
-// Give edges to the cube
-const edges1 = new THREE.EdgesGeometry(geometry1);
-const line1 = new THREE.LineSegments(
-  edges1,
-  new THREE.LineBasicMaterial({ color: 0x000000 })
-);
-scene.add(line1);
-
-// // Create a dashed line
-// const materia2 = new THREE.LineDashedMaterial({ color: 0x0000ff });
-// const points = [];
-// points.push(new THREE.Vector3(-10, 0, 0));
-// points.push(new THREE.Vector3(0, 10, 0));
-// points.push(new THREE.Vector3(10, 0, 0));
-// const geometry2 = new THREE.BufferGeometry().setFromPoints(points);
-// const line2 = new THREE.Line(geometry2, materia2);
-// scene.add(line2);
-
-// Making the cube clickable
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
-
-// Making some text
-// const loader = new FontLoader();
-// loader.load('fonts/helvetiker_regular.typeface.json', function (font) {
-//   const geometry = new TextGeometry("I'm a cube?", {
-//     font: font,
-//     size: 1,
-//     height: 1,
-//   });
-//   const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-//   const text = new THREE.Mesh(geometry, material);
-//   scene.add(text);
-// });
-
-// Making some text appear over a cube when it is clicked
-// window.addEventListener('click', (event) => {
-//   // Convert mouse position to normalized device coordinates (-1 to +1)
-//   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-//   // Update the picking ray with the camera and mouse position
-//   raycaster.setFromCamera(mouse, camera);
-
-//   // Calculate objects intersecting the picking ray
-//   const intersects = raycaster.intersectObjects(scene.children);
-
-//   for (let i = 0; i < intersects.length; i++) {
-//     if (intersects[i].object === cube) {
-//       alert("I'm a cube?");
-//     }
-//   }
-// });
-
-// Animation function for updating the scene
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  line1.rotation.x += 0.01;
-  line1.rotation.y += 0.01;
-  renderer.render(scene, camera);
-}
-
-// Run animation with webgl compatibility check
-if (WebGL.isWebGLAvailable()) {
-  // Initiate function or other initializations here
-  animate();
-} else {
-  const warning = WebGL.getWebGLErrorMessage();
-  document.getElementById('container').appendChild(warning);
-}
-
-/*
-
-Begin tensorflow.js section for learning
-
-*/
-const weight = Array.from(model.getWeights()[0].dataSync());
-const bias = Array.from(model.getWeights()[1].dataSync());
-
-console.log(model);
-console.log(`Model weight: ${weight}`);
-console.log(`Model bias: ${bias}`);
-window.model = model;
-
-/*
-
-Drawing a model (no weights for now)
-
-*/
-window.convModel = convModel;
-const weightsArray = convModel.getWeights()[0].dataSync();
-const kernelHeight = 5;
-const kernelWidth = 5;
-const inputChannels = 1;
-const outputChannels = 8;
-
-// Reshaping the array into the 4D shape
-const weights4D = tf.tensor(weightsArray, [
-  kernelHeight,
-  kernelWidth,
-  inputChannels,
-  outputChannels,
-]);
-
-// Accessing the kernel for each filter:
-for (let i = 0; i < outputChannels; i++) {
-  const kernel = weights4D.slice(
-    [0, 0, 0, i],
-    [kernelHeight, kernelWidth, inputChannels, 1]
-  );
-  // kernel now contains the 5x5 weights for the i-th filter
-  let kernelArray = kernel.arraySync();
-  let kernelString = kernelArray.map((row) => row.join(' ')).join('\n');
-  console.log(`Printing the ${i}th kernel:`);
-  console.log(kernelString);
-}
-
-/*
-Getting something from the user
-*/
-// Get the input and error message elements
-let inputElement = document.getElementById('user-input');
-let errorMessageElement = document.getElementById('error-message');
-
-let fontLoader = new FontLoader();
-let textMesh;
-
-function createTextMesh(text, font) {
-  let geometry = new TextGeometry(text, {
-    font: font,
-    size: 1,
-    height: 0.2,
-  });
-
-  let material = new THREE.MeshBasicMaterial({ color: 0x000000 });
-
-  let mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(-5, 2, 0);
-  // mesh.rotation.set(0, -Math.PI / 2, 0);
-
-  return mesh;
-}
-
-function updateSceneText(newText, font) {
-  // Remove the old text mesh from the scene
-  if (textMesh) {
-    scene.remove(textMesh);
-  }
-
-  // Create a new text mesh
-  textMesh = createTextMesh(newText, font);
-
-  // Add the new text mesh to the scene
-  scene.add(textMesh);
-}
-
-// fontLoader.load('fonts/helvetiker_regular.typeface.json', (font) => {
-//   // Handle the input event
-//   inputElement.addEventListener('input', (event) => {
-//     let userInput = event.target.value;
-
-//     // Check if the input is a number
-//     if (isNaN(userInput)) {
-//       errorMessageElement.textContent = 'Please enter a number.';
-//     } else {
-//       errorMessageElement.textContent = '';
-//       updateSceneText(userInput, font);
-//     }
-//   });
-// });
 
 // Adding a way for the user to draw
 // Initialize the drawing mode
@@ -244,25 +63,6 @@ function renderCanvasFromArray() {
   }
 }
 
-// Function to reset the canvas array and render it
-function resetCanvas() {
-  for (let y = 0; y < canvasArray.length; y++) {
-    for (let x = 0; x < canvasArray[y].length; x++) {
-      canvasArray[y][x] = 0;
-    }
-  }
-  renderCanvasFromArray();
-}
-
-// Function to update the brush size
-function updateBrushSize() {
-  brushSize = parseInt(this.value);
-}
-
-function toggleDrawingMode() {
-  drawingMode = this.checked ? 'erase' : 'draw';
-}
-
 function drawPixel(e) {
   if (!drawing) return;
 
@@ -288,6 +88,25 @@ function drawPixel(e) {
   renderCanvasFromArray();
 }
 
+// Function to reset the canvas array and render it
+function resetCanvas() {
+  for (let y = 0; y < canvasArray.length; y++) {
+    for (let x = 0; x < canvasArray[y].length; x++) {
+      canvasArray[y][x] = 0;
+    }
+  }
+  renderCanvasFromArray();
+}
+
+// Function to update the brush size
+function updateBrushSize() {
+  brushSize = parseInt(this.value);
+}
+
+function toggleDrawingMode() {
+  drawingMode = this.checked ? 'erase' : 'draw';
+}
+
 function setupEventListeners() {
   canvas.addEventListener('mousedown', (e) => {
     drawing = true;
@@ -304,3 +123,107 @@ function setupEventListeners() {
 // Initialization
 initializeCanvas();
 setupEventListeners();
+
+/*
+  Visualizing the network
+*/
+
+function getColor(value) {
+  return new THREE.Color(0.2 + value * 0.5, 0.6 + value * 0.4, 1 - value * 0.3);
+}
+
+const cubeSize = 0.05; // You can adjust this to fit the grid
+const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+const cubeMaterials = new Array(28)
+  .fill(null)
+  .map(() => new Array(28).fill(null));
+
+function createCubes() {
+  for (let i = 0; i < 28; i++) {
+    for (let j = 0; j < 28; j++) {
+      const value = canvasArray[i][j];
+      const color = getColor(value);
+      const material = new THREE.MeshBasicMaterial({ color });
+
+      const cube = new THREE.Mesh(geometry, material);
+      cube.position.set(
+        (j - 14) * cubeSize * 1.1,
+        (14 - i) * cubeSize * 1.1,
+        0
+      ); // Adjust for rotation
+      scene.add(cube);
+
+      cubeMaterials[i][j] = material;
+    }
+  }
+}
+
+createCubes();
+
+function updateCubeColors() {
+  for (let i = 0; i < 28; i++) {
+    for (let j = 0; j < 28; j++) {
+      const value = canvasArray[i][j];
+      const color = getColor(value);
+      cubeMaterials[i][j].color.set(color);
+    }
+  }
+}
+
+// Load the trained mnist model
+const savedModelPath = 'model/model.json';
+const model = await tf.loadLayersModel(savedModelPath);
+window.model = model;
+
+const inputTensor = tf.tensor(canvasArray, [28, 28, 1]).expandDims(0).toFloat();
+
+// Collect all the intermediate layers
+const layerOutputs = [];
+for (const layer of model.layers) {
+  layerOutputs.push(layer.output);
+}
+// Define a new model that outputs the intermediate activations
+const activationModel = tf.model({
+  inputs: model.input,
+  outputs: layerOutputs,
+});
+window.activationModel = activationModel;
+// Use the new model to predict the intermediate activations for a given input
+const activations = activationModel.predict(inputTensor);
+window.activations = activations;
+
+function createConvLayer(layerIndex, offsetX, offsetY, offsetZ) {
+  const activations = window.activations[layerIndex]; // Get activations for the specified layer
+  const [batchSize, height, width, numFilters] = activations.shape;
+
+  for (let k = 0; k < numFilters; k++) {
+    // Iterate through filters
+    for (let i = 0; i < height; i++) {
+      // Iterate through height
+      for (let j = 0; j < width; j++) {
+        // Iterate through width
+        const value = activations.arraySync()[0][i][j][k]; // Get activation value
+        const color = getColor(value);
+        const material = new THREE.MeshBasicMaterial({ color });
+
+        const cube = new THREE.Mesh(geometry, material);
+        cube.position.set(
+          offsetX + j * cubeSize * 1.5,
+          offsetY + (height - i) * cubeSize * 1.5,
+          offsetZ + k * cubeSize * 2 // Separate different filters in the Z-direction
+        );
+        scene.add(cube);
+      }
+    }
+  }
+}
+
+createConvLayer(0, 0, -1, 1);
+
+// run the animation to do updates
+function animate() {
+  updateCubeColors();
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
--- model.js
+++ model.js
@@ -1,7 +1,9 @@
-const convModel = tf.sequential();
+import * as tf from '@tensorflow/tfjs-node';
+
+const model = tf.sequential();
 
 // First convolutional layer with 8 filters of size 5x5 and input shape of 28x28 (assuming 28x28 image)
-convModel.add(
+model.add(
   tf.layers.conv2d({
     inputShape: [28, 28, 1],
     filters: 8,
@@ -11,10 +13,10 @@ convModel.add(
 );
 
 // First max-pooling layer with 2x2 window
-convModel.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
+model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
 
 // Second convolutional layer with 16 filters of size 3x3
-convModel.add(
+model.add(
   tf.layers.conv2d({
     filters: 16,
     kernelSize: 3,
@@ -23,15 +25,15 @@ convModel.add(
 );
 
 // Second max-pooling layer with 2x2 window
-convModel.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
+model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
 
 // Flatten the output before passing to fully connected layers
-convModel.add(tf.layers.flatten());
+model.add(tf.layers.flatten());
 
 // Fully connected layer with 32 units
-convModel.add(tf.layers.dense({ units: 32, activation: 'relu' }));
+model.add(tf.layers.dense({ units: 32, activation: 'relu' }));
 
 // Fully connected layer with 10 units (output layer)
-convModel.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
+model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
 
-export default convModel;
+export default model;
